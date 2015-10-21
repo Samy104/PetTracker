8b7fda38a3a40b6778cdc225483ff5cec9f70eb8
Samy Lemcelli <samy104@gmail.com>
2015-10-21 16:41:52 -0400

    Small modifications to the Module class

--- app/src/main/java/com/example/roadrunner/pettracker/model/Module.java
+++ app/src/main/java/com/example/roadrunner/pettracker/model/Module.java
@@ -1,7 +1,9 @@
 package com.example.roadrunner.pettracker.model;
 
 import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.field.ForeignCollectionField;
 import com.j256.ormlite.table.DatabaseTable;
+import java.util.Random;
 
 /**
  * Created by gnut3ll4 on 14/10/15.
@@ -15,7 +17,15 @@ public class Module {
     @DatabaseField
     private String name;
 
+    @DatabaseField(foreign = true, foreignAutoCreate = true, foreignAutoRefresh = true)
+    private Coordonnees currentPosition;
+
     public Module() {
+        currentPosition = new Coordonnees(45.521705, -73.577743);
+    }
+
+    public Module(Coordonnees initial) {
+        currentPosition = initial;
     }
 
     public Module(String name) {
@@ -30,7 +40,23 @@ public class Module {
         return name;
     }
 
+    public Coordonnees getCurrentPosition() { return currentPosition; }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    //This method will randomize a position relative to the current position
+    public void generateNewPosition()
+    {
+        Random ran = new Random();
+        double x = currentPosition.getLongitude();
+        double y = currentPosition.getLatitude();
+
+        double deltaX = x + ran.nextDouble()/1000;
+        double deltaY = y + ran.nextDouble()/1000;
+
+        currentPosition.setLongitude(x + deltaX);
+        currentPosition.setLatitude(y + deltaY);
     }
 }
