a8441c9f44996815ae6988d42d38bf562846f6a5
gnut3ll4 <thibaut.tauveron@gmail.com>
2015-10-31 04:53:18 -0400

    Displaying zone in manager

--- app/src/main/java/com/example/roadrunner/pettracker/ui/fragments/ZoneManagerFragment.java
+++ app/src/main/java/com/example/roadrunner/pettracker/ui/fragments/ZoneManagerFragment.java
@@ -1,74 +1,75 @@
 package com.example.roadrunner.pettracker.ui.fragments;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
 import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.roadrunner.pettracker.R;
+import com.example.roadrunner.pettracker.model.Module;
+import com.example.roadrunner.pettracker.model.Zone;
 import com.example.roadrunner.pettracker.ui.activities.ZoneAdderActivity;
+import com.example.roadrunner.pettracker.utils.DatabaseHelper;
+import com.example.roadrunner.pettracker.utils.MapsHelper;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.LatLng;
+import com.j256.ormlite.dao.Dao;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ZoneManagerFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ZoneManagerFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class ZoneManagerFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ZoneManagerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ZoneManagerFragment newInstance(String param1, String param2) {
-        ZoneManagerFragment fragment = new ZoneManagerFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    public ZoneManagerFragment() {
-        // Required empty public constructor
-    }
+    private DatabaseHelper databaseHelper;
+    private MapView mapView;
+    private GoogleMap googleMap;
+    private Dao<Zone, ?> zoneDao;
+    private ArrayList<Zone> zones;
+    private Dao<Module, ?> moduleDao;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_zone_manager, container, false);
+        View view = inflater.inflate(R.layout.fragment_zone_manager, container, false);
 
-        FloatingActionButton floatingActionButton = (FloatingActionButton) v.findViewById(R.id.fab_add_zone);
+        databaseHelper = new DatabaseHelper(getActivity());
+        mapView = (MapView) view.findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+
+        if (mapView != null) {
+            googleMap = mapView.getMap();
+            googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+            googleMap.setMyLocationEnabled(true);
+            googleMap.getUiSettings().setZoomControlsEnabled(true);
+
+            LatLng coordinate = new LatLng(45.494441, -73.560595);
+            MapsInitializer.initialize(getActivity());
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinate, 15));
+
+            try {
+                zoneDao = databaseHelper.getDao(Zone.class);
+                zones = new ArrayList<>(zoneDao.queryForAll());
+                MapsHelper.displayZones(googleMap, zones);
+                moduleDao = databaseHelper.getDao(Module.class);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        FloatingActionButton floatingActionButton = (FloatingActionButton) view.findViewById(R.id.fab_add_zone);
 
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,7 +80,28 @@ public class ZoneManagerFragment extends Fragment {
         });
 
 
-        return v;
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+
+        mapView.onLowMemory();
     }
 
 
